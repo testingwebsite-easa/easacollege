5813c85cdc85e2e8101011cde7b31028b2805131
testingwebsite-easa <testingwebsite@ecetonline.com>
2026-07-30 15:35:05 +0530

    feat: implement global CSS framework with Outfit font integration and theme support

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const asyncCssPlugin = () => {
+  return {
+    name: 'async-css-plugin',
+    transformIndexHtml(html) {
+      return html.replace(
+        /<link rel="stylesheet" crossorigin href="(\/assets\/[^"]+\.css)">/g,
+        '<link rel="preload" as="style" href="$1" /><link rel="stylesheet" href="$1" media="print" onload="this.media=\'all\'" /><noscript><link rel="stylesheet" href="$1" /></noscript>'
+      );
+    }
+  };
+};
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), asyncCssPlugin()],
   build: {
     rollupOptions: {
       output: {
